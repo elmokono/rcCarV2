917f3e14584282c06e0d5b59aec5a5365261e395
Daniel Carlozzi <elmokono@hotmail.com>
2023-06-17 01:04:50 -0300

    MINIMIZED

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,90 @@
-#include <Wire.h>
+// #include <Wire.h>
 #include <SPI.h>
 #include <Arduino.h>
 #include <RH_ASK.h>
 #include <Servo.h>
 
-int rfPin = 28;
-int servoPin = 27;
+// raspberry pi pico
+#define GAS_PIN 26
+#define SERVO_PIN 27
+#define RX_PIN 28
 
-RH_ASK driver(2000, rfPin, 29);
-Servo myservo;
+#define SERVO_DELAY_MS 15
+
+RH_ASK driver(2000, RX_PIN, 20);
+Servo servo;
 
 void setup()
 {
   Serial.begin(9600); // Debugging only
+
   if (!driver.init())
-    Serial.println("init failed");
+  {
+    pinMode(PICO_DEFAULT_LED_PIN, OUTPUT);
+    while (true)
+    {
+      digitalWrite(PICO_DEFAULT_LED_PIN, HIGH);
+      delay(500);
+      digitalWrite(PICO_DEFAULT_LED_PIN, LOW);
+      delay(500);
+    }
+  }
 
-  myservo.attach(servoPin);
-  myservo.write(90);
-  delay(15);
+  servo.attach(SERVO_PIN);
+  servo.write(90);
+  delay(50);
 
-  pinMode(PICO_DEFAULT_LED_PIN, OUTPUT);
+  pinMode(GAS_PIN, OUTPUT);
+
+  digitalWrite(PICO_DEFAULT_LED_PIN, HIGH);
+  delay(1000);
   digitalWrite(PICO_DEFAULT_LED_PIN, LOW);
+
+  Serial.println("RC Car V2 ready");
+}
+
+void checkGas(char command)
+{
+  if (command == 'F')
+  {
+    analogWrite(GAS_PIN, 255);
+  }
+  if (command == 'S')
+  {
+    analogWrite(GAS_PIN, 0);
+  }
+}
+
+void checkServo(char command)
+{
+  if (command == 'L')
+  {
+    servo.write(40);
+    delay(SERVO_DELAY_MS);
+  }
+  if (command == 'R')
+  {
+    servo.write(140);
+    delay(SERVO_DELAY_MS);
+  }
 }
 
 void loop()
 {
-  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+  uint8_t buf[1];
   uint8_t buflen = sizeof(buf);
 
   if (driver.recv(buf, &buflen)) // Non-blocking
   {
+    digitalWrite(PICO_DEFAULT_LED_PIN, HIGH);
+
     char command = ((char *)buf)[0];
-    // Message with a good checksum received, dump it.'
+    Serial.println(command);
 
-    Serial.print("Received(");
-    Serial.print(buflen);
-    Serial.print(",");
-    Serial.print(command);
-    Serial.println(")");
-    // driver.printBuffer(buf, buf, buflen);
+    checkGas(command);
 
-    if (command == 'L')
-    {
-      myservo.write(40);
-      delay(15);
-    }
-        if (command == 'R')
-    {
-      myservo.write(140);
-      delay(15);
-    }
+    checkServo(command);
+
+    digitalWrite(PICO_DEFAULT_LED_PIN, LOW);
   }
 }
