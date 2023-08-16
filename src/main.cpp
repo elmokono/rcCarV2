ad22cdf72614fc84c96e6a4585cc9414d139a31a
Daniel Carlozzi <elmokono@hotmail.com>
2023-08-16 20:19:55 -0300

    fixed steering calculation

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 /*
+This is for CAR
+
 ---------------------------
 | MISO | SCK  | CE  | GND |
 | #    | MOSI | CNS | VCC |
@@ -24,20 +26,24 @@ const byte address[6] = "00010";
 #define CS_PIN 21
 
 #define SERVO_PIN 27
-#define SERVO_DELAY_MS 15
+#define SERVO_DELAY_MS 50
 
 #define GAS_PIN 26
-#define GAS_LIMIT 128
+// #define GAS_LIMIT 128
+
+#define SERVO_MAX_ANGLE 35
 
 RF24 radio(CE_PIN, CS_PIN);
 Servo servo;
 unsigned long lastGasCheck = 0;
 uint8_t lastGas = 0;
 uint8_t lastSteer = 0;
+unsigned long lastServo = 0;
+unsigned long lastAlive = 0;
+int aliveLedState = LOW;
 
-void setup()
+void initRadio()
 {
-  Serial.begin(9600); // Debugging only
   if (!radio.begin())
   {
     Serial.println("Failed to init radio");
@@ -50,24 +56,38 @@ void setup()
       delay(50);
     }
   }
-  
+
   radio.setPALevel(RF24_PA_MIN);
   radio.setDataRate(RF24_250KBPS);
   radio.setChannel(124);
   radio.openReadingPipe(0, address);
   radio.setAutoAck(true);
   radio.startListening();
+}
+
+void setup()
+{
+  Serial.begin(9600); // Debugging only
+  initRadio();
 
   servo.attach(SERVO_PIN);
+  servo.write(90+SERVO_MAX_ANGLE);
+  delay(500);
+  servo.write(90-SERVO_MAX_ANGLE);
+  delay(500);
   servo.write(90);
-  delay(SERVO_DELAY_MS);
+  lastServo = millis();
 
   pinMode(GAS_PIN, OUTPUT);
-  lastGasCheck = millis();
+  lastGasCheck = lastAlive = millis();
 
-  digitalWrite(PICO_DEFAULT_LED_PIN, HIGH);
-  delay(5000);
-  digitalWrite(PICO_DEFAULT_LED_PIN, LOW);
+  for (int i = 0; i < 5; i++)
+  {
+    delay(100);
+    digitalWrite(PICO_DEFAULT_LED_PIN, HIGH);
+    delay(1000);
+    digitalWrite(PICO_DEFAULT_LED_PIN, LOW);
+  }
 
   Serial.println("RC Car V3 ready");
 }
@@ -85,24 +105,30 @@ void checkGas(uint8_t gasLevel)
   analogWrite(GAS_PIN, gasLevel);
 #endif
   lastGas = gasLevel;
-  Serial.println(gasLevel);
+  // Serial.println(gasLevel);
 }
 
 void checkServo(uint8_t steerLevel)
 {
-  if (steerLevel == lastSteer)
+  //no changes in steer or minimun time for servo has not been reached
+  if (steerLevel == lastSteer || millis() - lastServo < SERVO_DELAY_MS)
   {
     return;
   }
+
   // Serial.print("',steer:");
   // Serial.print(steerLevel);
 
   // 0 >= steerLevel <= 255
+  // SERVO_MAX_ANGLE >= steerLevel <= 100+SERVO_MAX_ANGLE
   // angle between 40 and 140
-  int value = (steerLevel * 100.0 / 255.0);
-  servo.write(value + 40);
+
+//50 - 90 - 130
+
+  int value = (steerLevel * SERVO_MAX_ANGLE * 2 / 255.0);
+  servo.write(value + 90 - SERVO_MAX_ANGLE);
   lastSteer = steerLevel;
-  Serial.println(value + 40);
+  lastServo = millis();
 }
 
 void loop()
@@ -112,7 +138,7 @@ void loop()
     uint8_t buf[32];
     radio.read(&buf, sizeof(buf));
     digitalWrite(PICO_DEFAULT_LED_PIN, HIGH);
-    Serial.println("command received");
+    // Serial.println("command received");
 
     // buff[0]=0x0
     // buff[1]=0-255
@@ -127,12 +153,19 @@ void loop()
   }
   else
   {
-    // no signal, slow down to zero
+    // signal lost, slow down to zero
     if (millis() - lastGasCheck > 250)
     {
       checkGas(0);
     }
   }
 
-  delay(SERVO_DELAY_MS);
+  if (millis() - lastAlive > 2000)
+  {
+    aliveLedState = aliveLedState == LOW ? HIGH : LOW;
+    digitalWrite(PICO_DEFAULT_LED_PIN, aliveLedState);
+    lastAlive = millis();
+  }
+
+  delay(10);
 }
