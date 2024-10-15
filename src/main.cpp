672024965edfa3b77d3d85f058c4995b5179903b
Daniel (legion) <elmokono@hotmail.com>
2024-10-15 15:57:16 -0300

    working servo and led

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,51 @@
+#include <Arduino.h>
+#include <RF24.h>
+#include "RP2040_ISR_Servo.h"
+#include  <Adafruit_NeoPixel.h>
+
+#define LED_PIN 16 //rp2040 zero
+Adafruit_NeoPixel led(1, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 /*
 This is for CAR
+*/
 
+/*
 ---------------------------
 | MISO | SCK  | CE  | GND |
 | #    | MOSI | CNS | VCC |
 |                         |
 |        (ANTENNA)        |
 ---------------------------
+//rp2040 zero spi0 pins
+//---------------------------
+MISO = SPI RX - receiver
+MOSI = SPI TX - transmitter (not used)
+SCK = SPI Clock
+CE = Custom PIN on RP2040
+CNS = Custom PIN on RP2040
+//---------------------------
+//#define PIN_SPI_MISO  (0u)
+//#define PIN_SPI_SS    (1u)
+//#define PIN_SPI_SCK   (2u)
+//#define PIN_SPI_MOSI  (3u)
 */
-/*
-pico spi0 pins
----------------------------
-#define PIN_SPI_MISO  (16u)
-#define PIN_SPI_MOSI  (19u)
-#define PIN_SPI_SCK   (18u)
-#define PIN_SPI_SS    (17u)
-*/
-
-#include <Arduino.h>
-#include <Servo.h>
-#include <RF24.h>
-
 const byte address[6] = "00010";
-#define CE_PIN 22
-#define CS_PIN 21
+#define CE_PIN 8
+#define CS_PIN 7
 
-#define SERVO_PIN 27
+#define SERVO_PIN 0
 #define SERVO_DELAY_MS 50
+#define MIN_MICROS 800
+#define MAX_MICROS 2450
 
-#define GAS_PIN 26
-// #define GAS_LIMIT 128
+#define GAS_PIN 1
+//#define GAS_LIMIT 1750
 
 #define SERVO_MAX_ANGLE 35
+#define SERVO_REF_ANGLE 45
 
 RF24 radio(CE_PIN, CS_PIN);
-Servo servo;
 unsigned long lastGasCheck = 0;
 uint8_t lastGas = 0;
 uint8_t lastSteer = 0;
@@ -42,19 +53,20 @@ unsigned long lastServo = 0;
 unsigned long lastAlive = 0;
 int aliveLedState = LOW;
 
+
+void doLed(int r, int g, int b)
+{
+  led.setPixelColor(0, led.Color(r,g,b,255)); 
+  led.show();
+}
+
 void initRadio()
 {
   if (!radio.begin())
   {
     Serial.println("Failed to init radio");
-    pinMode(PICO_DEFAULT_LED_PIN, OUTPUT);
-    while (true)
-    {
-      digitalWrite(PICO_DEFAULT_LED_PIN, HIGH);
-      delay(50);
-      digitalWrite(PICO_DEFAULT_LED_PIN, LOW);
-      delay(50);
-    }
+    doLed(255,0,0);
+    while(true){};
   }
 
   radio.setPALevel(RF24_PA_MIN);
@@ -67,29 +79,32 @@ void initRadio()
 
 void setup()
 {
+  led.begin();  
+  delay(2000);
   Serial.begin(9600); // Debugging only
-  initRadio();
+  Serial.println("init..");
+  doLed(255,255,0);
 
-  servo.attach(SERVO_PIN);
-  servo.write(90+SERVO_MAX_ANGLE);
+  initRadio();
+  Serial.println("radio ok");
+  
+  pinMode(SERVO_PIN, OUTPUT);
+  digitalWrite(SERVO_PIN, LOW);
+  RP2040_ISR_Servos.setupServo(SERVO_PIN, MIN_MICROS, MAX_MICROS);
+  RP2040_ISR_Servos.setPosition(SERVO_PIN, SERVO_REF_ANGLE+SERVO_MAX_ANGLE);
   delay(500);
-  servo.write(90-SERVO_MAX_ANGLE);
+  RP2040_ISR_Servos.setPosition(SERVO_PIN, SERVO_REF_ANGLE-SERVO_MAX_ANGLE);
   delay(500);
-  servo.write(90);
+  RP2040_ISR_Servos.setPosition(SERVO_PIN, SERVO_REF_ANGLE);
   lastServo = millis();
+  Serial.println("servo ok");
 
   pinMode(GAS_PIN, OUTPUT);
   lastGasCheck = lastAlive = millis();
 
-  for (int i = 0; i < 5; i++)
-  {
-    delay(100);
-    digitalWrite(PICO_DEFAULT_LED_PIN, HIGH);
-    delay(1000);
-    digitalWrite(PICO_DEFAULT_LED_PIN, LOW);
-  }
+  doLed(0,255,0);
 
-  Serial.println("RC Car V3 ready");
+  Serial.println("RC Car V4 ready");
 }
 
 void checkGas(uint8_t gasLevel)
@@ -102,7 +117,8 @@ void checkGas(uint8_t gasLevel)
 #ifdef GAS_LIMIT
   analogWrite(GAS_PIN, gasLevel > GAS_LIMIT ? GAS_LIMIT : gasLevel);
 #else
-  analogWrite(GAS_PIN, gasLevel);
+  int escGas = map(gasLevel, 0, 255, 1500, 2000);
+  analogWrite(GAS_PIN, escGas);
 #endif
   lastGas = gasLevel;
   // Serial.println(gasLevel);
@@ -126,7 +142,7 @@ void checkServo(uint8_t steerLevel)
 //50 - 90 - 130
 
   int value = (steerLevel * SERVO_MAX_ANGLE * 2 / 255.0);
-  servo.write(value + 90 - SERVO_MAX_ANGLE);
+  RP2040_ISR_Servos.setPosition(SERVO_PIN, value + SERVO_REF_ANGLE - SERVO_MAX_ANGLE);
   lastSteer = steerLevel;
   lastServo = millis();
 }
@@ -137,8 +153,12 @@ void loop()
   {
     uint8_t buf[32];
     radio.read(&buf, sizeof(buf));
-    digitalWrite(PICO_DEFAULT_LED_PIN, HIGH);
-    // Serial.println("command received");
+    digitalWrite(LED_PIN, HIGH);
+    /*Serial.print("command received ");
+    Serial.print(buf[0]);Serial.print(" ");
+    Serial.print(buf[1]);Serial.print(" ");
+    Serial.print(buf[2]);Serial.print(" ");
+    Serial.print(buf[3]);Serial.println();*/
 
     // buff[0]=0x0
     // buff[1]=0-255
@@ -148,7 +168,7 @@ void loop()
     checkGas(buf[1]);
     checkServo(buf[2]);
 
-    digitalWrite(PICO_DEFAULT_LED_PIN, LOW);
+    digitalWrite(LED_PIN, LOW);
     lastGasCheck = millis();
   }
   else
@@ -163,7 +183,7 @@ void loop()
   if (millis() - lastAlive > 2000)
   {
     aliveLedState = aliveLedState == LOW ? HIGH : LOW;
-    digitalWrite(PICO_DEFAULT_LED_PIN, aliveLedState);
+    digitalWrite(LED_PIN, aliveLedState);
     lastAlive = millis();
   }
 
